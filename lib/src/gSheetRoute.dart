85a09e5a8a224e3d99c0ab9388d50311baf3605f
XavierChanth <xchanthavong@gmail.com>
2021-06-29 00:32:43 -0400

    fixes

--- lib/src/gSheetRoute.dart
+++ lib/src/gSheetRoute.dart
@@ -31,7 +31,7 @@ class GSheetRoute {
     final router = Router();
 
     // This will error out since it tries to map to itself.
-    router.get('/column/0', (req) => Response.notFound('Not Found'));
+    router.get('/column/0>', (req) => Response.notFound('Not Found'));
 
     router.get('/column/<column|[0-9]+>', (req, String column) async {
       try {
@@ -39,7 +39,7 @@ class GSheetRoute {
         var data = await _worksheet(worksheetId)
             .values
             .map
-            .column(int.parse(column), fromRow: _firstDataRow!);
+            .column(int.parse(column)+1, fromRow: _firstDataRow!);
         return Response.ok(jsonEncode(data),
             headers: {'content-type': 'application/json'});
       } catch (e) {
