040f0242d85e02128902bdf7f4b0a79038a98bf5
XavierChanth <xchanthavong@gmail.com>
2021-06-28 22:52:33 -0400

    update routes file

--- lib/src/routes.dart
+++ lib/src/routes.dart
@@ -1,13 +1,20 @@
 import 'package:shelf/shelf.dart';
 import 'package:shelf_router/shelf_router.dart';
-import 'routes/index.dart';
+import 'gSheetRoute.dart';
 
 class Routes {
   Handler get handler {
     final router = Router();
 
-    router.mount('/taxonomy', Taxonomy().router);
-    router.mount('/dev-errors', DevErrors().router);
+    // router.mount('/taxonomy', GSheetRoute(_spreadsheetId, _firstDataRow, _worksheets));
+
+    // router.mount('/dev-errors', GSheetRoute(_spreadsheetId, _firstDataRow, _worksheets));
+
+    // Example
+    // router.mount('/example', GSheetRoute(_spreadsheetId, _firstDataRow, _worksheets));
+    // String _spreadsheetId: id of the spreadsheet
+    // int _firstDataRow: row of the first data (after the header)
+    // List<String> _worksheets: ids of all the worksheets to include in the api
 
     return router;
   }
--- lib/src/routes/index.dart
+++
@@ -1,2 +0,0 @@
-export 'taxonomy.dart';
-export 'devErrors.dart';
